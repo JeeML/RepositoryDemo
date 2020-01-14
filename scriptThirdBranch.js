51989d456e7a185222be157d0a8620c4b31a3b15
JeeML <jean-mich.lemaire@hotmail.com>
2020-01-13 22:21:19 -0500

    ajout var a scriptThirdBranch

--- scriptThirdBranch.js
+++ scriptThirdBranch.js
@@ -1,3 +1,4 @@
-alert("I'm building a 3rd branch only to practice git);
-alert("I really like learning how to code, it's like a puzzle all day long")
+alert("I'm building a 3rd branch only to practice git");
+alert("I really like learning how to code, it's like a puzzle all day long");
+var text = "Still adding some text for fun";
 
