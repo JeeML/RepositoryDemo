97d276f94756c9a54e0f358e83b8822965b420d2
JeeML <jean-mich.lemaire@hotmail.com>
2020-01-14 21:53:59 -0500

    modif de demo

--- demoScript.js
+++ demoScript.js
@@ -3,12 +3,6 @@ var hello="Hello World"
 alert(hello)
 
 var test = "modification sur les deux branches en meme temps);
-=======
-var hello="Hello World";
-
-alert(hello);
-
-
 
 var test2 = "test avec nom different"
->>>>>>> secondBranch
+
